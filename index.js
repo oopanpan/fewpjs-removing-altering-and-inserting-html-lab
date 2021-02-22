dacea8e41439903b1ba2e0009e95916508bcfaa1
oopanpan <oopanpan@gmail.com>
2021-02-22 11:06:50 -0800

    commit tree 5a9b02bc367ef14c475edf81f1e5d23e0dd28f23

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code here!
+let main = document.querySelector('main#main');
+main.remove();
+let newHeader = document.createElement('h1');
+// or_this  newHeader.setAttribute('id','victory');
+newHeader.id = 'victory';
+document.body.appendChild(newHeader);
+newHeader.innerHTML = "YOUR-NAME is the champion";
